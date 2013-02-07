4fa02e3277fbc533153bceb22a606dfa3d296b7e
Bartosz Jankiewicz <bartosz.jankiewicz@gmail.com>
2013-02-08 00:38:17 +0100

    Generic pictures upload

--- app/controllers/FileController.scala
+++ app/controllers/FileController.scala
@@ -12,6 +12,7 @@ import views.html.defaultpages.notFound
 import play.api.mvc.ChunkedResult
 import play.api.mvc.ResponseHeader
 import play.api.libs.iteratee.Iteratee
+import play.api.libs.Files.TemporaryFile
 
 object FileController extends Controller {
 
@@ -41,8 +42,30 @@ object FileController extends Controller {
     }
   }
   
+  def pictures = UploadPictures { implicit request =>
+    Logger.info("Uploading pictures")
+    Ok(s"uploaded ${request.files.size} files")
+  }
+
   def add = Action { implicit request =>
     Ok(views.html.admin.uploadfile.apply)
   }
 
+  def UploadPictures(f: FileRequest => Result) = {
+    Action(parse.multipartFormData) { request =>
+      val ids = request.body.files.filter(!_.filename.isEmpty()) map { picture =>
+        Logger.info(s"Saving ${picture.filename}")
+        val filename = picture.filename
+        val contentType = picture.contentType
+        File.store(picture.filename, "unknown", picture.ref.file)
+      }
+      f(FileRequest(ids, request))
+    }
+  }
+  
 }
+
+  case class FileRequest(
+    files: Seq[UUID],
+    private val request: Request[MultipartFormData[TemporaryFile]]) extends WrappedRequest(request)
+
